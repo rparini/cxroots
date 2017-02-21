0c3ccb24211662bf7fcaee151f0b3306636bb32b
Robert Parini <robert.parini@gmail.com>
2017-02-21 22:40:25 +0000

    Update docs

--- docs/_static/bizstyle.js
+++ docs/_static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("cxroots 0.2.0 documentation");
+        $("li.nav-item-0 a").text("cxroots 0.4.0 documentation");
     }
 });
