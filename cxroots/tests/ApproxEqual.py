c56d94d7ad6e8d8c4907028fbfdbd1d2b4428af7
Robert Parini <robert.parini@gmail.com>
2017-09-26 21:00:05 +0100

    Rename SetsApproxEqual to ApproxEqual

--- cxroots/tests/ApproxEqual.py
+++ cxroots/tests/ApproxEqual.py
@@ -22,3 +22,19 @@ def sets_approx_equal(a, b, decimal=7):
 
 	# use numpy to compare the two arrays element-wise 
 	return np.testing.assert_almost_equal(a, b, decimal)
+
+
+def roots_approx_equal(a, b, decimal = 7):
+	a_roots, a_multiplicities = a
+	b_roots, b_multiplicities = b
+
+	a_roots, b_roots = np.array(a_roots), np.array(b_roots)
+	a_multiplicities, b_multiplicities = np.array(a_multiplicities), np.array(b_multiplicities)
+
+	for ai, a_element in np.ndenumerate(a_roots):
+		bi = np.argmin(np.abs(b_roots - a_element))
+		b_roots[ai], b_roots[bi] = b_roots[bi], b_roots[ai]
+		b_multiplicities[ai], b_multiplicities[bi] = b_multiplicities[bi], b_multiplicities[ai]
+
+	np.testing.assert_almost_equal(a_roots, b_roots, decimal)
+	np.testing.assert_almost_equal(a_multiplicities, b_multiplicities, decimal)
