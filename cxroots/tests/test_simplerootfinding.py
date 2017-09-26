d55635b37e076b4e7a3d240bceb5880834252272
Robert Parini <robert.parini@gmail.com>
2017-09-26 20:52:49 +0100

    Change test names

--- cxroots/tests/test_simplerootfinding.py
+++ cxroots/tests/test_simplerootfinding.py
@@ -5,11 +5,7 @@ from scipy import pi
 from cxroots import Circle, Rectangle, PolarRect
 from cxroots.tests.SetsApproxEqual import sets_approx_equal
 
-class TestSimpleRootfinding(unittest.TestCase):
-	"""
-	Rootfinding but only simple roots
-	"""
-
+class TestRootfindingPolynomial(unittest.TestCase):
 	def setUp(self):
 		self.roots = roots = [-1.234, 0,  1+1j, 1-1j, 2.345]
 		self.f  = lambda z: (z-roots[0])*(z-roots[1])*(z-roots[2])*(z-roots[3])*(z-roots[4])
@@ -19,27 +15,27 @@ class TestSimpleRootfinding(unittest.TestCase):
 		self.Rectangle = Rectangle([-2,2],[-2,2])
 		self.halfAnnulus = PolarRect(0, [0.5,3], [-pi/2, pi/2])
 
-	def test_rootfindingPolynomial_circle_fdf(self):
+	def test_rootfinding_polynomial_circle_fdf(self):
 		approxRoots, multiplicities = self.Circle.roots(self.f, self.df)
 		sets_approx_equal(approxRoots, self.roots, decimal=7)
 
-	def test_rootfindingPolynomial_circle_f(self):
+	def test_rootfinding_polynomial_circle_f(self):
 		approxRoots, multiplicities = self.Circle.roots(self.f)
 		sets_approx_equal(approxRoots, self.roots, decimal=7)
 
-	def test_rootfindingPolynomial_rectangle_fdf(self):
+	def test_rootfinding_polynomial_rectangle_fdf(self):
 		approxRoots, multiplicities = self.Rectangle.roots(self.f, self.df)
 		sets_approx_equal(approxRoots, self.roots[:-1], decimal=7)
 
-	def test_rootfindingPolynomial_rectangle_f(self):
+	def test_rootfinding_polynomial_rectangle_f(self):
 		approxRoots, multiplicities = self.Rectangle.roots(self.f)
 		sets_approx_equal(approxRoots, self.roots[:-1], decimal=7)
 
-	def test_rootfindingPolynomial_halfAnnulus_fdf(self):
+	def test_rootfinding_polynomial_halfAnnulus_fdf(self):
 		approxRoots, multiplicities = self.halfAnnulus.roots(self.f, self.df)
 		sets_approx_equal(approxRoots, self.roots[2:], decimal=7)
 
-	def test_rootfindingPolynomial_halfAnnulus_f(self):
+	def test_rootfinding_polynomial_halfAnnulus_f(self):
 		approxRoots, multiplicities = self.halfAnnulus.roots(self.f)
 		sets_approx_equal(approxRoots, self.roots[2:], decimal=7)
 
