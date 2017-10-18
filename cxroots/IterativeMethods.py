50af26306c4729634b5fd3ff510214814b178564
Robert Parini <robert.parini@gmail.com>
2017-10-18 17:34:35 +0100

    Use Muller's method

--- cxroots/IterativeMethods.py
+++ cxroots/IterativeMethods.py
@@ -16,26 +16,101 @@ def iterateToRoot(x0, f, df=None, steptol=1e-12, roottol=1e-12, maxIter=20):
 		except (RuntimeError, OverflowError):
 			return None
 	else:
-		# Secant method: 
-		x1, x2 = x0, x0 + (x0+1)*1e-8
-		root, err = secant(x1, x2, f, steptol, 0, maxIter)
+		# fcalls = [0,0]
+		# def secant_f(z):
+		# 	fcalls[0] += 1
+		# 	return f(z)
+		# def muller_f(z):
+		# 	fcalls[1] += 1
+		# 	return f(z)
 
-		# XXX: Secant method is very slow to converge.  Use Muller's method instead?
-		# Muller's method: uses 3 initial points
-		# git+git://github.com/mnishida/PyMuller@master#egg=pymuller
-		# from pymuller import Muller
-		# muller = Muller(f, 1, args=None, xis=[x0], dxs=[1e-4*(1+x0)], xtol=steptol, ftol=0, maxiter=maxIter)
-		# muller()
-		# root = muller.roots[0]
-		# err = abs(root)
+		# # Secant method: 
+		# x1, x2 = x0, x0 + (x0+1)*1e-8
+		# root, err = secant(x1, x2, secant_f, steptol, 0, maxIter)
+		# print('-----')
+		# print('Secant:', fcalls[0], root, err)
 
-		# import mpmath # XXX: mpmath insists on functions accepting mpc which is inconvenient
-		# x1, x2, x3 = x0, x0*(1 + 1e-4) + 1e-4, x0*(1 + 2e-4) + 2e-4
-		# root = mpmath.findroot(f, (x1, x2, x3), solver='muller', tol=roottol, verbose=False, verify=False)
-		# err = abs(root)
+		# # Muller's method:
+		# x1, x2, x3 = x0, x0*(1 + 1e-8) + 1e-8, x0*(1 - 1e-8) - 1e-8
+		# root, err = muller(x1, x2, x3, muller_f, steptol, 0, maxIter)
+		# print('Muller:', fcalls[1], root, err)
+		# print('-----')
+
+		root, err = muller(x1, x2, x3, f, steptol, 0, maxIter)
 
 	if err < roottol:
 		return root
+
+def muller(x1, x2, x3, f, steptol=1e-12, roottol=1e-12, maxIter=20, verbose=False, callback=None):
+	"""
+	Wrapper for mpmath's implementation of Muller's method.  
+
+	Parameters
+	----------
+	x1 : float or complex
+		An initial point for iteration, should be close to a 
+		root of f.
+	x2 : float or complex
+		An initial point for iteration, should be close to a 
+		root of f.  Should not equal x1.
+	x3 : float or complex
+		An initial point for iteration, should be close to a 
+		root of f.  Should not equal x1 or x2.
+	f : function
+		Function of a single variable f(x)
+	steptol: float, optional
+		Routine will end if the step size, dx, between sucessive
+		iterations of x satisfies abs(dx) < steptol
+	roottol: float, optional
+		The routine will end if abs(f(x)) < roottol
+	maxIter : int, optional
+		Routine ends after maxIter iterations
+	callback : function, optional
+		After each iteration the supplied function 
+		callback(x, dx, f(x), iteration) will be called where 'x' is the current iteration 
+		of the estimated root, 'dx' is the step size between the previous 
+		and current 'x' and 'iteration' the number of iterations that have been taken.  
+		If the callback function evaluates to True then the routine will end
+
+	Returns
+	-------
+	xf : float
+		The approximation to a root of f
+	rooterr : float
+		The error of the original function at xf, abs(f(xf))
+
+	"""
+	from mpmath import mp, mpmathify
+	from mpmath.calculus.optimization import Muller
+
+
+	# mpmath insists on functions accepting mpc
+	f_mpmath = lambda z: mpmathify(f(complex(z))) 
+
+	mull = Muller(mp, f_mpmath, (x1, x2, x3), verbose=verbose)
+	iteration = 0
+	x0 = x3
+
+	try:
+		for x, err in mull:
+			dx = x - x0
+
+			if callback is not None and callback(x, dx, err, iteration+1):
+				break
+
+			if abs(dx) < steptol or err < roottol or iteration > maxIter:
+				break
+
+			iteration += 1
+			x0 = x
+
+	except ZeroDivisionError:
+		# ZeroDivisionError comes up if the error is evaluated to be zero
+		pass
+
+	# cast mpc and mpf back to regular complex and float
+	return complex(x), float(err)
+
 
 def newton(x0, f, df, steptol=1e-12, roottol=1e-12, maxIter=20, callback=None):
 	"""
