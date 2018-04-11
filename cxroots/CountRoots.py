1deedf62aefdabddd2e979430b149daed1c41a0c
Robert Parini <robert.parini@gmail.com>
2018-04-11 13:05:06 +0100

    Fix typo

--- cxroots/CountRoots.py
+++ cxroots/CountRoots.py
@@ -92,7 +92,7 @@ def prod(C, f, df=None, phi=None, psi=None, absTol=1e-12, relTol=1e-12, divMin=5
 	elif method == 'quad':
 		if approx_df:
 			import numdifftools
-			dx = numdifftools.Derivative(f, order=m)
+			df = numdifftools.Derivative(f, order=m)
 			# df = lambda z: scipy.misc.derivative(f, z, dx=1e-8, n=1, order=3)
 			# df = CxDeriv(f) # too slow
 
