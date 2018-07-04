fe8b6507ecf17d395c534ac54ca7540ed4cbcd78
Robert Parini <robert.parini@gmail.com>
2018-07-04 09:35:38 +0100

    Tidy test_cluster

--- cxroots/tests/test_cluster.py
+++ cxroots/tests/test_cluster.py
@@ -1,12 +1,10 @@
 import unittest
 import pytest
 import numpy as np
-from scipy import pi, sqrt, exp, sin, cos
 
 from cxroots import Circle, Rectangle
 from cxroots.tests.ApproxEqual import roots_approx_equal
 
-### XXX: Need some way to distinguish clusters of roots
 class ClusterTest(object):
 	def test_rootfinding_df_smallStop(self):
 		roots_approx_equal(self.C.roots(self.f, self.df, verbose=True, errStop=0), (self.roots, self.multiplicities), decimal=12)
@@ -14,8 +12,8 @@ class ClusterTest(object):
 	def test_rootfinding_df(self):
 		roots_approx_equal(self.C.roots(self.f, self.df, verbose=True), (self.roots, self.multiplicities), decimal=12)
 
-	# def test_rootfinding_f(self):
-	# 	roots_approx_equal(self.C.roots(self.f), (self.roots, self.multiplicities), decimal=12)
+	def test_rootfinding_f(self):
+		roots_approx_equal(self.C.roots(self.f), (self.roots, self.multiplicities), decimal=12)
 
 class TestCluster_1_rect(unittest.TestCase, ClusterTest):
 	def setUp(self):
@@ -71,7 +69,7 @@ class TestCluster_3_circle(unittest.TestCase, ClusterTest):
 		self.f = lambda z: np.prod([z-r for r in roots])
 		self.df = lambda z: np.sum([np.prod([z-r for r in np.delete(roots,i)]) for i in range(len(roots))])
 
-# Need to handle tight clusters of roots better
+# XXX: Need to handle tight clusters of roots better
 @pytest.mark.xfail()
 class TestCluster_4_rect(unittest.TestCase, ClusterTest):
 	def setUp(self):
@@ -95,15 +93,3 @@ class TestCluster_4_circle(unittest.TestCase, ClusterTest):
 
 if __name__ == '__main__':
 	unittest.main(verbosity=3)
-
-	# roots = [3, 3.00001, 3.00002, 8, 8.00002, 8+0.00001j]
-	# multiplicities = [1,1,1,1,1,1]
-
-	# f = lambda z: np.prod([z-r for r in roots])
-	# df = lambda z: np.sum([np.prod([z-r for r in np.delete(roots,i)]) for i in range(len(roots))])
-
-	# C = Circle(0, 8.5)
-	# # C = Rectangle([2,9], [-1,1])
-
-	# # C.approximate_roots(f, df, verbose=True)
-	# C.demo_roots(f, df, verbose=True)
