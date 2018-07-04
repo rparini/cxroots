90febd71bba8f36f29a56e7da49527046bf4573d
Robert Parini <robert.parini@gmail.com>
2018-07-04 12:43:45 +0100

    Remove CxDeriv import

--- cxroots/CountRoots.py
+++ cxroots/CountRoots.py
@@ -5,8 +5,6 @@ import scipy.integrate
 import scipy.misc
 import warnings
 
-from .CxDerivative import CxDeriv
-
 def prod(C, f, df=None, phi=None, psi=None, absTol=1e-12, relTol=1e-12, divMin=5, divMax=10, m=2, method='quad', verbose=False, callback=None, integerTol=inf):
 	r"""
 	Compute the symmetric bilinear form used in (1.12) of [KB]
@@ -119,7 +117,6 @@ def prod(C, f, df=None, phi=None, psi=None, absTol=1e-12, relTol=1e-12, divMin=5
 		if approx_df:
 			df = numdifftools.Derivative(f, order=m)
 			# df = lambda z: scipy.misc.derivative(f, z, dx=1e-8, n=1, order=3)
-			# df = CxDeriv(f) # too slow
 
 		I, err = 0, 0
 		for segment in C.segments:
