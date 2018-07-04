4fc3c2fb96efb6d0973d29edb8722fdcfac690b2
Robert Parini <robert.parini@gmail.com>
2018-07-04 20:48:21 +0100

    Add reson to cluster xfail

--- cxroots/tests/test_cluster.py
+++ cxroots/tests/test_cluster.py
@@ -66,8 +66,7 @@ class TestCluster_3_circle(unittest.TestCase, ClusterTest):
 		self.f = lambda z: np.prod([z-r for r in roots])
 		self.df = lambda z: np.sum([np.prod([z-r for r in np.delete(roots,i)]) for i in range(len(roots))])
 
-# XXX: Need to handle tight clusters of roots better
-@pytest.mark.xfail()
+@pytest.mark.xfail(reason='Cluster of roots too tight')
 class TestCluster_4_rect(unittest.TestCase, ClusterTest):
 	def setUp(self):
 		self.roots = roots = [3, 3.00001, 3.00002, 8, 8.00002, 8+0.00001j]
@@ -77,7 +76,7 @@ class TestCluster_4_rect(unittest.TestCase, ClusterTest):
 		self.f = lambda z: np.prod([z-r for r in roots])
 		self.df = lambda z: np.sum([np.prod([z-r for r in np.delete(roots,i)]) for i in range(len(roots))])
 
-@pytest.mark.xfail()
+@pytest.mark.xfail(reason='Cluster of roots too tight')
 class TestCluster_4_circle(unittest.TestCase, ClusterTest):
 	def setUp(self):
 		self.roots = roots = [3, 3.00001, 3.00002, 8, 8.00002, 8+0.00001j]
