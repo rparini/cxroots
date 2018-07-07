19ec0db6b3629998bd6d63e9d51b0d932a139984
Robert Parini <robert.parini@gmail.com>
2018-07-07 13:27:15 +0100

    Fix import

--- cxroots/__init__.py
+++ cxroots/__init__.py
@@ -5,7 +5,7 @@ from .Contours.Rectangle import Rectangle
 
 from .RootFinder import findRoots
 from .DemoRootFinder import demo_findRoots
-from .CountRoots import count_enclosed_roots
+from .CountRoots import count_roots
 from .Derivative import CxDerivative
 from .Derivative import get_multiplicity
 
