0a0f115f50d029f3cf1edca73b599b0dffeda7a8
Robert Parini <robert.parini@gmail.com>
2018-07-08 17:52:16 +0100

    Include contours folder in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 from setuptools.command.test import test as TestCommand # Need for 'test' command to be recognised
 from numpy.distutils.misc_util import get_numpy_include_dirs
 
-packages = ['cxroots', 'cxroots.tests']
+packages = ['cxroots', 'cxroots.tests', 'cxroots.contours']
 
 # get the version
 exec(open('cxroots/version.py').read())
