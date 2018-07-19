ead544fd4806e9cd41d757912081b1e4c64f299e
Robert Parini <robert.parini@gmail.com>
2018-07-19 17:30:44 +0100

    Tidy up FOP generation

--- cxroots/ApproximateRoots.py
+++ cxroots/ApproximateRoots.py
@@ -156,52 +156,42 @@ def approximate_roots(C, N, f, df=None, absTol=1e-12, relTol=1e-12, errStop=1e-8
 				print('G ', G[:p+1,:p+1])
 				print('G1', G1[:p+1,:p+1])
 
-			# The regular FOP only exists if H is non-singular.
-			# An alternate citeration given by [KB] is to proceed as if it is regular and
-			# then compute its zeros.  If any are arbitary or infinite then this
-			# polynomial should instead be defined as an inner polynomial.
-			# Here, an inner polynomial is defined if any of the computed
-			# roots are outside of the interior of the contour.
+			"""
+			If any of the zeros of the FOP are outside of the interior of 
+			the contour then we assume that they are 'arbitary' and 
+			instead define the FOP as an inner polynomial. [KB]
+			"""
 			polyRoots = scipy.linalg.eig(G1[:p+1,:p+1], G[:p+1,:p+1])[0]+mu
 			if np.all([C.contains(z) for z in polyRoots]):
-				# define a regular polynomial
-				phiZeros.append(polyRoots)
 				r, t = r+t+1, 0
+				phiZeros.append(polyRoots)
 
-				if verbose:
-					print('Regular poly', r+t, 'roots:', phiZeros[-1])
+				if verbose: print('Regular poly', r+t, 'roots:', phiZeros[-1])
 
-				# if any of these elements are not small then continue
-				allSmall = True
+				# is the number of distinct roots, n=r?
 				phiFuncLast = phiFunc(-1)
 				for j in range(N-r):
 					ip, err = product(lambda z: phiFuncLast(z)*(z-mu)**j, phiFuncLast)
 
-					# if not small then carry on
-					if verbose:
-						print(j, 'of', N-r, 'stop?', abs(ip) + err)
-					### XXX: Use the 'maxpsum' estimate for precision loss in [KB]?
-					if abs(ip) + err > errStop:
-						allSmall = False
+					if verbose: print(j, 'of', N-r, 'stop?', abs(ip))
+					if abs(ip) > errStop:
+						# n != r so carry on
 						break
-
-				if allSmall:
-					# all the roots have been found
-					break
+				else:
+					# the for loop did not break
+					continue
+				break
 
 			else:
+				# define an inner polynomial as phi_{r+t+1} = phi_{t+1} phi_{r}
 				t += 1
-
-				# define an inner polynomial phi_{r+t+1} = phi_{t+1} phi_{r}
 				phiZeros.append(np.append(phiZeros[t],phiZeros[r]))
-
-				if verbose:
-					print('Inner poly', r+t, 'roots:', phiZeros[-1])
+				if verbose: print('Inner poly', r+t, 'roots:', phiZeros[-1])
 
 		roots = np.array(phiZeros[-1])
 
 		if verbose:
-			print('Roots:')
+			print('Computed Roots:')
 			print(roots)
 
 		# remove any roots which are not distinct
@@ -209,14 +199,12 @@ def approximate_roots(C, N, f, df=None, absTol=1e-12, relTol=1e-12, errStop=1e-8
 		for i, root in enumerate(roots):
 			if len(roots[i+1:]) > 0 and np.any(np.abs(root-roots[i+1:]) < rootTol):
 				rootsToRemove.append(i)
-
 		roots = np.delete(roots, rootsToRemove)
 		n = len(roots)
 
 		# compute the multiplicities, eq. (1.19) in [KB]
 		V = np.column_stack([roots**i for i in range(n)])
-		if verbose and n > 2:
-			print('Computing ordinary moments')
+		if verbose and n > 2: print('Computing ordinary moments')
 		s += [product(lambda z: z**p)[0] for p in range(2, n)]
 		multiplicities = np.dot(s[:n], np.linalg.inv(V))
 
@@ -224,7 +212,9 @@ def approximate_roots(C, N, f, df=None, absTol=1e-12, relTol=1e-12, errStop=1e-8
 		### better than np.dot(s, np.linalg.inv(V)).  Especially since we know
 		### the result must be an integer anyway.
 		# import vandermonde
-		# multiplicities = vandermonde.solve_transpose(np.array(roots), np.array(s))
+		# multiplicities_vandermonde = vandermonde.solve_transpose(np.array(roots), np.array(s))
+		# print('multi  ', multiplicities)
+		# print('multi V', multiplicities_vandermonde)
 
 		### Note that n = rank(H_N) is not used since calculating the
 		### rank of a matrix of floats appears to be quite unstable
