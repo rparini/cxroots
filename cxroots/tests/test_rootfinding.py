6238b05b6b945c613edc9fbd523b114a717472c3
Robert Parini <robert.parini@gmail.com>
2018-07-30 15:19:40 +0100

    Move TestConjugate

--- cxroots/tests/test_rootfinding.py
+++ cxroots/tests/test_rootfinding.py
@@ -201,51 +201,6 @@ class TestRootfinding_153(unittest.TestCase, RootfindingTests, MultiplicityTests
 					  -0.4607141197289707542294459477 + 0.6254277693477682516688207854j]
 		self.multiplicities = [3,2,1,1,1]
 
-class TestConjugate(unittest.TestCase):
-	def setUp(self):
-		self.C = Circle(0, 1.5)
-		self.f = lambda z: z**27-2*z**11+0.5*z**6-1
-		self.df = lambda z: 27*z**26-22*z**10+3*z**5
-		self.symmetry = lambda z: [z.conjugate()]
-
-		self.roots = [-1.03509521179240, 
-					  -0.920332541459108, 
-					  1.05026721944263, 
-					  -0.983563736801535 - 0.382365167035741j, 
-					  -0.983563736801535 + 0.382365167035741j, 
-					  -0.792214346729517 - 0.520708613101932j, 
-					  -0.792214346729517 + 0.520708613101932j, 
-					  -0.732229626596468 - 0.757345327222341j, 
-					  -0.732229626596468 + 0.757345327222341j, 
-					  -0.40289002582335 - 0.825650446354661j, 
-					  -0.40289002582335 + 0.825650446354661j, 
-					  -0.383382611408318 - 0.967939747947639j, 
-					  -0.383382611408318 + 0.967939747947639j, 
-					  -0.02594227096144 - 1.05524415820652j, 
-					  -0.02594227096144 + 1.05524415820652j, 
-					  0.160356899544475 - 0.927983420797727j, 
-					  0.160356899544475 + 0.927983420797727j, 
-					  0.41133738621461 - 0.967444751898913j, 
-					  0.41133738621461 + 0.967444751898913j, 
-					  0.576737152896681 - 0.719511178392941j, 
-					  0.576737152896681 + 0.719511178392941j, 
-					  0.758074415348703 - 0.724716122470435j, 
-					  0.758074415348703 + 0.724716122470435j, 
-					  0.903278407433416 - 0.22751872334709j, 
-					  0.903278407433416 + 0.22751872334709j, 
-					  0.963018623787179 - 0.427294816877434j, 
-					  0.963018623787179 + 0.427294816877434j]
-		
-		self.multiplicities = np.ones_like(self.roots)
-
-	def test_conjugate_realPoly_df(self):
-		roots = self.C.roots(self.f, self.df, verbose=True, guessRootSymmetry = self.symmetry)
-		roots_approx_equal(roots, (self.roots, self.multiplicities), decimal=12)
-
-	def test_conjugate_realPoly_f(self):
-		roots = self.C.roots(self.f, verbose=True, guessRootSymmetry = self.symmetry)
-		roots_approx_equal(roots, (self.roots, self.multiplicities), decimal=12)
-
 def test_reevaluation_of_N():
 	from cxroots import Circle
 	C = Circle(0,2)
@@ -269,7 +224,6 @@ class TestIntroduction(unittest.TestCase, RootfindingTests, MultiplicityTests):
 					  0.64857808095387581293067569277 - 1.35662268398824203963215495605j,
 					  0.64857808095387581293067569277 + 1.35662268398824203963215495605j]
 		self.multiplicities = [3,2,1,1,1,1,1]
-
 
 if __name__ == '__main__':
 	unittest.main(verbosity=3)
