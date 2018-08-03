c90ce40b8f92e16e10277913c6b25f8a280187aa
Robert Parini <robert.parini@gmail.com>
2018-08-03 20:15:21 +0100

    Update docs

--- docs/ex_annular_combustion-1.py
+++ docs/ex_annular_combustion-1.py
@@ -10,5 +10,5 @@ f = lambda z: z**2 + A*z + B*exp(-T*z) + C
 df = lambda z: 2*z + A - B*T*exp(-T*z)
 
 rectangle = Rectangle([-15000,5000], [-15000,15000])
-roots = rectangle.roots(f, df)
+roots = rectangle.roots(f, df, rootErrTol=1e-6)
 roots.show()
