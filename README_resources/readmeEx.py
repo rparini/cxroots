a66110cd427fe4196b4e915ec4278307c54761af
Robert Parini <robert.parini@gmail.com>
2018-08-03 13:11:45 +0100

    create_readme -> create_resources

--- README_resources/readmeEx.py
+++ README_resources/readmeEx.py
@@ -4,6 +4,6 @@ f = lambda z: (exp(2*z)*cos(z)-1-sin(z)+z**5)*(z*(z+2))**2
 from cxroots import Circle
 C = Circle(0,3)
 roots = C.roots(f)
-roots.show('README_resources/readmeEx.png')
+roots.show('readmeEx.png')
 
 print(roots)
