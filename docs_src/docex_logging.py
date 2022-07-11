84b9c2b5e77d6c0021f727d93006cf75a4df22a3
Robert Parini <robert.parini@gmail.com>
2022-07-11 10:34:48 +0100

    Update docs_src/docex_logging.py

--- docs_src/docex_logging.py
+++ docs_src/docex_logging.py
@@ -5,5 +5,6 @@ from cxroots import Circle
 C = Circle(0, 3)
 f = lambda z: (exp(-z) * sin(z / 2) - 1.2 * cos(z)) * (z + 1.2) * (z - 2.5) ** 2
 
-logging.basicConfig(level=logging.INFO)
+# use logging.DEBUG to show diagnostic information
+logging.basicConfig(level=logging.INFO) 
 C.roots(f)
