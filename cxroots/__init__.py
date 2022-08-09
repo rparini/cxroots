c7fdd85f3d438eb8ef7f2346c555255b920a2ffb
Robert Parini <robert.parini@gmail.com>
2022-08-09 14:47:37 +0100

    Revert "Rename version.py to fix setup.cfg"
    This reverts commit 84a139dfeb9b9a29daff49b09185bbeea4ae3cc0.

--- cxroots/__init__.py
+++ cxroots/__init__.py
@@ -1,4 +1,3 @@
-from .__version__ import __version__  # noqa:F401
 from .contours.annulus import Annulus
 from .contours.annulus_sector import AnnulusSector
 from .contours.circle import Circle
@@ -7,6 +6,7 @@ from .derivative import cx_derivative, find_multiplicity
 from .root_counting import count_roots
 from .root_finding import find_roots
 from .root_finding_demo import demo_find_roots
+from .version import __version__  # noqa:F401
 
 # Define public interface
 __all__ = [
