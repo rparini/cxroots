ff15b17f010673cd9aa42392bef67c7a1fc88e69
Robert Parini <robert.parini@gmail.com>
2022-08-09 17:00:47 +0100

    Remove source_suffix comments

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -54,10 +54,7 @@ extensions = [
 templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
-# You can specify multiple suffix as a list of string:
-#
-# source_suffix = ['.rst', '.md']
-source_suffix = ".rst"
+source_suffix = [".rst"]
 
 # The master toctree document.
 master_doc = "index"
