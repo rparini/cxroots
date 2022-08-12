9900be307c336088c57fdb27064e7cb8eab8675f
Robert Parini <robert.parini@gmail.com>
2022-08-12 12:53:24 +0100

    Update test_secant.py

--- cxroots/tests/test_secant.py
+++ cxroots/tests/test_secant.py
@@ -18,7 +18,7 @@ def test_secant():
     def callback(x, dx, y, iteration):
         return iterations.append(x)
 
-    x, _ = secant(0.5, pi / 4, f, callback=callback)
+    x, _ = secant(0.5, pi / 4, f, callback=callback, root_tol=1e-12)
     iterations.append(x)
 
     correct_iterations = [
