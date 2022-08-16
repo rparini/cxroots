23ed7f98284d651dd5006b796188e9c0124f0dc9
Robert Parini <robert.parini@gmail.com>
2022-08-16 11:15:18 +0100

    Parameterise contour call tests

--- cxroots/tests/test_contours.py
+++ cxroots/tests/test_contours.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 
 from cxroots import AnnulusSector, Circle, Rectangle
 
@@ -10,41 +11,39 @@ def test_annulus_sector_contains():
     phi1 = 6.519
 
     z = 9 - 1.04825594683e-18j
-    contour = AnnulusSector(0, [r0, r1], [phi0, phi1])
+    contour = AnnulusSector(0, (r0, r1), (phi0, phi1))
 
     assert contour.contains(z)
 
 
 def test_rect_contains():
-    contour = Rectangle([-2355, -1860], [-8810, -8616])
+    contour = Rectangle((-2355, -1860), (-8810, -8616))
 
     assert contour.contains(-2258 - 8694j)
     assert not contour.contains(-2258 - 8500j)
 
 
-def test_rect_call():
-    c = Rectangle([0, 2], [0, 1])
-
-    t_arr = np.array([0, 0.125, 0.25, 0.5, 0.75, 1])
-    z_arr = np.array([0, 1, 2, 2 + 1j, 1j, 0])
-
+@pytest.mark.parametrize(
+    "contour,t_arr,z_arr",
+    [
+        pytest.param(
+            Rectangle((0, 2), (0, 1)),
+            np.array([0, 0.125, 0.25, 0.5, 0.75, 1]),
+            np.array([0, 1, 2, 2 + 1j, 1j, 0]),
+            id="Rectangle",
+        ),
+        pytest.param(
+            Circle(0, 1),
+            np.array([0, 0.25]),
+            np.array([1, 6.123233995736766e-17 + 1j]),
+            id="Circle",
+        ),
+    ],
+)
+def test_contour_call(contour, t_arr, z_arr):
     # Test passing in the whole array
-    assert np.all(c(t_arr) == z_arr)
+    assert np.all(contour(t_arr) == z_arr)
 
     # Test individual calls
     for t, z in zip(t_arr, z_arr):
-        assert c(t) == z
-
-
-def test_circle_call():
-    c = Circle(0, 1)
-
-    t_arr = np.array([0, 0.25])
-    z_arr = np.array([1, 6.123233995736766e-17 + 1j])
-
-    # Test passing in the whole array
-    assert np.all(c(t_arr) == z_arr)
-
-    # Test individual calls
-    for t, z in zip(t_arr, z_arr):
-        assert c(t) == z
+        assert contour(t) == z
